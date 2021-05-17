f707f218a4d2e1a846c36db3cf41c920c0172d3f
Mirko <mirkosbaglia2001@gmail.com>
2021-05-17 14:27:30 +0200

    Creazione file

---
+++ MyLaravelComics/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('home', 'TestController@home');
+Route::get('home2', 'TestController@home2');
