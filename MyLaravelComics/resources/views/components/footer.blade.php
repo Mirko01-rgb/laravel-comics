1190ce8b2308ad285bebae57e65865265848d648
Mirko <mirkosbaglia2001@gmail.com>
2021-05-17 22:57:32 +0200

    Footer up

--- MyLaravelComics/resources/views/components/footer.blade.php
+++ MyLaravelComics/resources/views/components/footer.blade.php
@@ -1,5 +1,126 @@
 <footer>
-  <div class="footer-up">
-    <img class="img-footer-logo"src="{{ asset('/storage/assets/dc-logo-bg.png') }}" alt="">
+  <div class="footer-up flex ">
+    <div class="box-listato ">
+      <div class="list flex flex-start ">
+        <div class="flex-column">
+          <ul>
+            <li class="padding">
+             <a class="white"  href="#">DC COMICS</a>
+            </li>
+            <li>
+             <a class="grey" href="#">Characters</a>
+            </li>
+            <li>
+             <a class="grey" href="#">Comics</a>
+            </li>
+            <li>
+             <a class="grey" href="#">Movies</a>
+            </li>
+            <li>
+             <a class="grey" href="#">TV</a>
+            </li>
+            <li>
+             <a  class="grey" href="#">Games</a>
+            </li>
+            <li>
+             <a class="grey" href="#">Video</a>
+            </li>
+            <li>
+             <a class="grey" href="#">News</a>
+            </li>
+          </ul>
+          <ul class="padding-list">
+            <li class="padding">
+             <a class="white"  href="#">SHOP</a>
+            </li>
+            <li>
+             <a class="grey" href="#">Shop Dc</a>
+            </li>
+            <li>
+             <a class="grey" href="#">Shop DC Collectibles</a>
+            </li>
+          </ul>
+        </div>
+        <ul class="padding-list">
+          <li class="padding">
+           <a class="white" href="#">DC</a>
+          </li>
+          <li>
+           <a class="grey" href="#">Terms of Use</a>
+          </li>
+          <li>
+           <a class="grey" href="#">Privacy policy (New)</a>
+          </li>
+          <li>
+           <a class="grey" href="#">Ad Choices</a>
+          </li>
+          <li>
+           <a class="grey" href="#">Advertising</a>
+          </li>
+          <li>
+           <a class="grey" href="#">Jobs</a>
+          </li>
+          <li>
+           <a class="grey" href="#">Subscriptions</a>
+          </li>
+          <li>
+           <a class="grey" href="#">Talent Workshops</a>
+          </li>
+          <li>
+           <a  class="grey" href="#">CPSC Certificates</a>
+          </li>
+          <li>
+           <a class="grey"  href="#">Ratings</a>
+          </li>
+          <li>
+           <a class="grey" href="#">Shop Help</a>
+          </li>
+          <li>
+           <a class="grey" href="#">Concact Us</a>
+          </li>
+        </ul>
+        <ul>
+          <li class="padding">
+           <a class="white" href="#">SITES</a>
+          </li>
+          <li>
+           <a class="grey" href="#">DC</a>
+          </li>
+          <li>
+           <a class="grey" href="#">MAD Magazine</a>
+          </li>
+          <li>
+           <a class="grey" href="#">DC KIDS</a>
+          </li>
+          <li>
+           <a class="grey" href="#">DC Universe</a>
+          </li>
+          <li>
+           <a class="grey" href="#">DC Power Visa</a>
+          </li>
+        </ul>
+      </div>
+      <div class="paragraph">
+        <p class=" grey">All Site Content TM and 2020 Dc Entertainment, unless otherwise <a href="#" class="blue">noted here</a>.
+         All rights reserved. <a class="blue" href="#">Cookies Settings</a>
+        </p>
+      </div>
+    </div>
+    <div class="box-img">
+     <img class="img-footer-logo"src="{{ asset('/storage/assets/dc-logo-bg.png') }}" alt="">
+
+    </div>
   </div>
+
+  <div class="footer-down">
+
+  </div>
+
+
+
+
+
+
+
+
 </footer>
